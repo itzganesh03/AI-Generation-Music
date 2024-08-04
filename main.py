8a91c8ba507ca5d5c10fc748dc2afcc5ef935afd
RajatVardam <89441086+RajatVardam@users.noreply.github.com>
2024-08-04 23:50:49 +0530

    g

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-if __name__ == "__main__":
-    app = FastAPI()
+app = FastAPI()
 
-
-    @app.get("/r")
-    def read_root():
-        return {"Hello": "World"}
+@app.get("/r")
+def read_root():
+    return {"Hello": "World"}
